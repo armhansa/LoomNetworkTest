5f0d1bba7372df9a27bf31440add782ecc031532
LAPTOP-80CBAS8U\Asus <armhansa@gmail.com>
2019-04-09 17:57:59 +0700

    Comment Get Decks change to get Matches (Bugs: dict just has 2 player's deck)

--- index.php
+++ index.php
@@ -20,44 +20,58 @@ include 'func/curl_func.php';
 		// $decksStat['id'] = ['name'=>'', 'user_id'=>'', 'win'=>0, 'loss'=>0, 'draw'=>0]; and total (cal from win+loss+draw)
 		// $cardsStat['id'] = ['name'=>'', 'win'=>0, 'loss'=>0, 'draw'=>0]; // and total (cal from win+loss+draw)
 	}
+	$decksStat = [];
 
 	// Get API to Decks Store
-	$decksTotal = getJson($urlDecks)['total'];
-	$pages = ceil($decksTotal/100);
-	for ($i=1; $i<=$pages; $i++) {
-		$jsonDecksPage = getJson("$urlDecks?page=$i");
-		foreach ($jsonDecksPage['decks'] as $deck) {
-			$decksStat[$deck['user_id'].":".$deck['deck_id']] = [
-				'id'=>$deck['id']
-				, 'name'=>$deck['name']
-				, 'win'=>0
-				, 'loss'=>0
-				, 'draw'=>0];
-		}
-	}
-
-	// Get API to Matches Store
-	// $matchesTotal = getJson($urlMatches)['total'];
-	// $pages = ceil($matchesTotal/100);
+	// $decksTotal = getJson($urlDecks)['total'];
+	// $pages = ceil($decksTotal/100);
 	// for ($i=1; $i<=$pages; $i++) {
-	// 	$jsonMatchesPage = getJson("$urlDecks?page=$i");
-	// 	foreach ($jsonMatchesPage['decks'] as $match) {
-	// 		if ($match['status'] == "Ended") {
-	// 			$player[] = [$match['player1_id'], $match['player1_deck_id']];
-	// 			$player[] = [$match['player2_id'], $match['player2_deck_id']];
-	// 			if ($match['winner_id'] == "") {
-	// 				$decksStat[$player[0][0].":".$player[0][1]]['draw']++;
-	// 				$decksStat[$player[1][0].":".$player[1][1]]['draw']++;
-	// 			} else if ($match['winner_id'] == $player[0][0]) {
-	// 				$decksStat[$player[0][0].":".$player[0][1]]['win']++;
-	// 				$decksStat[$player[1][0].":".$player[1][1]]['loss']++;
-	// 			} else {
-	// 				$decksStat[$player[0][0].":".$player[0][1]]['loss']++;
-	// 				$decksStat[$player[1][0].":".$player[1][1]]['win']++;
-	// 			}
-	// 		}
+	// 	$jsonDecksPage = getJson("$urlDecks?page=$i");
+	// 	foreach ($jsonDecksPage['decks'] as $deck) {
+	// 		$decksStat[$deck['user_id'].":".$deck['deck_id']] = [
+	// 			'id'=>$deck['id']
+	// 			, 'name'=>$deck['name']
+	// 			, 'win'=>0
+	// 			, 'loss'=>0
+	// 			, 'draw'=>0];
 	// 	}
 	// }
+
+	// Get API to Matches Store
+	$matchesTotal = getJson($urlMatches)['total'];
+	$pages = ceil($matchesTotal/100);
+	$pages = 1; // for test
+	for ($i=1; $i<=$pages; $i++) {
+		$jsonMatchesPage = getJson("$urlMatches?page=$i");
+		foreach ($jsonMatchesPage['matches'] as $match) {
+			$player[] = [$match['player1_id'], $match['player1_deck_id']];
+			$player[] = [$match['player2_id'], $match['player2_deck_id']];
+			// dict created
+			if (!array_key_exists($player[0][0].":".$player[0][1], $decksStat)) {
+				$decksStat[$player[0][0].":".$player[0][1]] = [
+					'win'=>0
+					, 'loss'=>0
+					, 'draw'=>0];
+			}
+			if (!array_key_exists($player[1][0].":".$player[1][1], $decksStat)) {
+				$decksStat[$player[1][0].":".$player[1][1]] = [
+					'win'=>0
+					, 'loss'=>0
+					, 'draw'=>0];
+			}
+			// Cal Stat
+			if ($match['winner_id'] == "") {
+				$decksStat[$player[0][0].":".$player[0][1]]['draw']++;
+				$decksStat[$player[1][0].":".$player[1][1]]['draw']++;
+			} else if ($match['winner_id'] == $player[0][0]) {
+				$decksStat[$player[0][0].":".$player[0][1]]['win']++;
+				$decksStat[$player[1][0].":".$player[1][1]]['loss']++;
+			} else {
+				$decksStat[$player[0][0].":".$player[0][1]]['loss']++;
+				$decksStat[$player[1][0].":".$player[1][1]]['win']++;
+			}
+		}
+	}
 
 ?>
 
@@ -74,12 +88,13 @@ include 'func/curl_func.php';
 		if (count($decksStat) > 0) {
 			echo "<table align='center'>";
 			// echo "<tr><th class='bolder'>Eng Name</th><th class='bolder'>Section</th></tr>";
-			echo "<tr> <th>USER_ID:Deck ID</th> <th>Deck ID</th> <th>Deck Name</th> <th>Win</th> <th>Loss</th> <th>Draw</th> <th>Total</th> </tr>";
+			// echo "<tr> <th>USER_ID:Deck ID</th> <th>Deck ID</th> <th>Deck Name</th> <th>Win</th> <th>Loss</th> <th>Draw</th> <th>Total</th> </tr>";
+			echo "<tr> <th>USER_ID:Deck ID</th> <th>Win</th> <th>Loss</th> <th>Draw</th> <th>Total</th> </tr>";
 			foreach ($decksStat as $key => $itemDeck) {
 				echo "<tr>";
 				echo "<td>".$key."</td>";
-				echo "<td>".$itemDeck['id']."</td>";
-				echo "<td>".$itemDeck['name']."</td>";
+				// echo "<td>".$itemDeck['id']."</td>";
+				// echo "<td>".$itemDeck['name']."</td>";
 				echo "<td>".$itemDeck['win']."</td>";
 				echo "<td>".$itemDeck['loss']."</td>";
 				echo "<td>".$itemDeck['draw']."</td>";
@@ -91,6 +106,8 @@ include 'func/curl_func.php';
 			echo "Error Decks in Database is not availiable!";
 			die();
 		}
+
+		print_r($decksStat);
 	?>
 </body>
 </html>
--- test.php
+++ test.php
@@ -3,5 +3,7 @@
 $decksStat['test'] = ['win'=>0, 'loss'=>2];
 $decksStat['test']['loss']++;
 print_r($decksStat);
+$decksStat['test2'] = ['win'=>0, 'loss'=>2];
+print_r($decksStat);
 
 ?>
