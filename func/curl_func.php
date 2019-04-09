12e7eb1412d0413d44a28ffc54d07c28ed42d252
LAPTOP-80CBAS8U\Asus <armhansa@gmail.com>
2019-04-09 15:31:39 +0700

    Create Func curl for easy usable.

---
+++ func/curl_func.php
@@ -0,0 +1,32 @@
+<?php
+
+function getContent($url) {
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => false,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+			"Content-Type: application/json"
+		),
+	));
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+	if ($err) {
+		echo "cURL Error #:" . $err;
+		die();
+	}
+	return $response;
+}
+
+function getJson($url) {
+	return json_decode(getContent($url), true);
+}
+
+?>
